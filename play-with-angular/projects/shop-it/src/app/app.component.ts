44233efbbe3216bc7df8fe4014ba8c43a5cbdd15
nag <nagtraininglab@gmail.com>
2023-07-27 21:39:54 +0530

    shop-IT version-1

--- play-with-angular/projects/shop-it/src/app/app.component.ts
+++ play-with-angular/projects/shop-it/src/app/app.component.ts
@@ -6,5 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'shop-IT';
+  products = [
+    {
+      id: 1,
+      name: 'Laptop',
+      price: 10000,
+      isAvailable: true,
+      description: 'Laptop Description',
+      imagePath: 'assets/images/Laptop.png'
+    },
+    // {
+    //   id: 2,
+    //   name: 'Mobile',
+    //   price: 1000,
+    //   isAvailable: true,
+    //   description: 'Mobile Description',
+    //   imagePath: 'assets/images/Mobile.png'
+    // }
+  ]
+
+  currentTab=1
+
+  isTabSelected(tabIndex:number){
+    return this.currentTab===tabIndex
+  }
+
+  handleBuy(event: MouseEvent) {
+    console.log("handling buy ");
+    console.log(event);
+  }
+  handleTabChange(event: MouseEvent,tabIndex:number) {
+    this.currentTab=tabIndex
+  }
+
 }
